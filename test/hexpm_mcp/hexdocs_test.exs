8ec428cc471d800793385669321a14f4e8f71314
Josh Rotenberg <joshrotenberg@gmail.com>
2026-04-05 17:08:46 -0700

    fix: improve HTML-to-markdown conversion for hexdocs pages
    - Target #content area instead of broad selectors
    - Strip navigation chrome (sidebar, search, footer, buttons, toasts)
    - Add table-to-markdown conversion (thead/tbody/tr/td/th)
    - Extract code language from class attributes (elixir, erlang, etc.)
    - Add blockquote, h4, hr support
    - Clean up redundant whitespace in output
    - Skip button, input, nav, footer elements during conversion
    
    Closes #24

--- test/hexpm_mcp/hexdocs_test.exs
+++ test/hexpm_mcp/hexdocs_test.exs
@@ -60,8 +60,66 @@ defmodule HexpmMcp.HexDocsTest do
       refute md =~ ".foo"
     end
 
+    test "converts tables to markdown" do
+      html = """
+      <table>
+        <thead><tr><th>Name</th><th>Value</th></tr></thead>
+        <tbody><tr><td>foo</td><td>1</td></tr><tr><td>bar</td><td>2</td></tr></tbody>
+      </table>
+      """
+
+      md = HexDocs.html_to_markdown(html)
+      assert md =~ "| Name | Value |"
+      assert md =~ "| --- | --- |"
+      assert md =~ "| foo | 1 |"
+      assert md =~ "| bar | 2 |"
+    end
+
+    test "converts blockquotes" do
+      html = "<blockquote><p>Important note</p></blockquote>"
+      md = HexDocs.html_to_markdown(html)
+      assert md =~ "> Important note"
+    end
+
+    test "extracts code language from class" do
+      html = ~S[<pre><code class="makeup elixir">IO.puts("hello")</code></pre>]
+      md = HexDocs.html_to_markdown(html)
+      assert md =~ "```elixir"
+    end
+
+    test "strips navigation chrome" do
+      html = """
+      <div id="content">
+        <nav><a href="/">Home</a></nav>
+        <p>Actual content</p>
+        <footer>Copyright</footer>
+      </div>
+      """
+
+      md = HexDocs.html_to_markdown(html)
+      assert md =~ "Actual content"
+      refute md =~ "Home"
+      refute md =~ "Copyright"
+    end
+
+    test "targets #content area" do
+      html = """
+      <html>
+        <body>
+          <nav id="sidebar">Sidebar stuff</nav>
+          <div id="content">
+            <p>Module documentation</p>
+          </div>
+        </body>
+      </html>
+      """
+
+      md = HexDocs.html_to_markdown(html)
+      assert md =~ "Module documentation"
+      refute md =~ "Sidebar stuff"
+    end
+
     test "handles plain text fallback on parse failure" do
-      # Not valid HTML but should not crash
       result = HexDocs.html_to_markdown("")
       assert is_binary(result)
     end
